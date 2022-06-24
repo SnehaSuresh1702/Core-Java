652eb3ab0f2ad6f14e520dc68cf183737c40a6f5
Sneha Suresh <Sneha Suresh@SNEHA-k-l>
2022-06-24 17:17:36 +0530

    2

---
+++ Arrays/src/ArraysEx2.java
@@ -0,0 +1,20 @@
+
+public class ArraysEx2 {
+
+	public static void main(String[] args) {
+		
+		int[][] a = new int[3][2];
+		//a[0] = new int[2];
+		//a[1] = new int[2];
+		//a[2] = new int[2];
+		
+		System.out.println(a[0][0]);
+		System.out.println(a[0][1]);
+		System.out.println(a[1][0]);
+		System.out.println(a[1][1]);
+		System.out.println(a[2][0]);
+		System.out.println(a[2][1]);
+		
+	}
+
+}
