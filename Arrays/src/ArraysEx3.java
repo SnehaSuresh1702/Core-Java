edb7a0a9637b10c779112694f6bb92c3c8ccfcb5
Sneha Suresh <Sneha Suresh@SNEHA-k-l>
2022-06-24 17:40:14 +0530

    3

--- Arrays/src/ArraysEx3.java
+++ Arrays/src/ArraysEx3.java
@@ -5,11 +5,17 @@ public class ArraysEx3 {
 		
 		int[][] a = new int[3][2];
 		
-		for(int i = 0; i < a.length; i++) {
+		/*for(int i = 0; i < a.length; i++) {
 			for(int j = 0; j < a[i].length; j++) {
 				System.out.println(a[i][j]);
+			}*/
+		
+		
+		// More Readable print statement
+		for(int i = 0; i < a.length; i++) {
+			for(int j = 0; j < a[i].length; j++) {
+				System.out.println("a[" + i + "][" + j + "]: " + a[i][j]);
 			}
-		}
+		}	
 	}
-
 }
