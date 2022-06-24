636565c513dd584174b4e3796b7bc97dffe2f476
Sneha Suresh <Sneha Suresh@SNEHA-k-l>
2022-06-24 17:34:08 +0530

    3

---
+++ Arrays/src/ArraysEx3.java
@@ -0,0 +1,15 @@
+
+public class ArraysEx3 {
+
+	public static void main(String[] args) {
+		
+		int[][] a = new int[3][2];
+		
+		for(int i = 0; i < a.length; i++) {
+			for(int j = 0; j < a[i].length; j++) {
+				System.out.println(a[i][j]);
+			}
+		}
+	}
+
+}
