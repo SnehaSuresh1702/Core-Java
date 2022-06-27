95720ff8496f4194dc080f908061709c0b94d183
Sneha Suresh <Sneha Suresh@SNEHA-k-l>
2022-06-27 12:06:03 +0530

    5

--- Challenges/src/Ch5Q2.java
+++ Challenges/src/Ch5Q2.java
@@ -3,14 +3,29 @@ public class Ch5Q2 {
 
 	public static void main(String[] args) {
 		
-		int[] a = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		/*int[] a = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 		
 		int j = 19;
-		for(int i = 1; i <= 10; i++) {
+		for(int i = 0; i <= 9; i++) {
+		
+			j = i * 19;
 			a[i] = j;
-			j = j * 19;
 			
 		}
+		
+		for(int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+			
+		}*/
+		
+int[] a = new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		
+		int j = 19;
+		for(int i = 0; i <= 9; i++) {
+		
+			j = (i+1) * 19;
+			a[i] = j;
+		}	
 		
 		for(int i = 0; i < a.length; i++) {
 			System.out.println(a[i]);
