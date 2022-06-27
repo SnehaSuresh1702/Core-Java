df2b33391f6317004bb435e668365c368a6873d9
Sneha Suresh <Sneha Suresh@SNEHA-k-l>
2022-06-27 13:54:15 +0530

    8

--- Challenges/src/Ch7Q2.java
+++ Challenges/src/Ch7Q2.java
@@ -6,12 +6,17 @@ public class Ch7Q2 {
 		int tableOf = 7;
 		int [] arr = {1, 2, 3, 4, 5, 6, 7 ,8, 9, 10};
 		
-		for(int i = 0; i < arr.length; i++ ) {
+		/*for(int i = 0; i < arr.length; i++ ) {
 			
 				System.out.println("7*" + arr[i] + "=" + 7*arr[i]);
 			
-			}
+		}*/
+		
+		for(int x : arr) {
 			
+			System.out.println("7*" + x + "=" + 7*x);
+		
+	}	
 		}
 	
 
