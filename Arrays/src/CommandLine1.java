7103d5c73144928ce3498d0e7a07abc78856ce8b
Sneha Suresh <Sneha Suresh@SNEHA-k-l>
2022-06-27 09:54:47 +0530

    3

---
+++ Arrays/src/CommandLine1.java
@@ -0,0 +1,13 @@
+
+public class CommandLine1 {
+
+	public static void main(String[] args) {
+
+		System.out.println(args.length);
+		
+		for(String val : args) {
+			System.out.println(val);
+		}
+	}
+
+}
