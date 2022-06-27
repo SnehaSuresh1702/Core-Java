e3c91a9d6da740e6fe16719c6bda7a6eb3b39e99
Sneha Suresh <Sneha Suresh@SNEHA-k-l>
2022-06-27 09:37:48 +0530

    2

---
+++ Arrays/src/ArraySizeZero.java
@@ -0,0 +1,11 @@
+
+public class ArraySizeZero {
+
+	public static void main(String[] args) {
+	
+		int [] arr = new int[0];
+		System.out.println("Array size is: " + arr.length);
+		//System.out.println(arr[0]); 
+	}
+
+}
