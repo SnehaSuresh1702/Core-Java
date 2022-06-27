f1294015621e132a98eba5fc0355e019b9498380
Sneha Suresh <Sneha Suresh@SNEHA-k-l>
2022-06-27 13:16:05 +0530

    8

---
+++ Challenges/src/Ch7Q2.java
@@ -0,0 +1,18 @@
+
+public class Ch7Q2 {
+
+	public static void main(String[] args) {
+
+		int tableOf = 7;
+		int [] arr = {1, 2, 3, 4, 5, 6, 7 ,8, 9, 10};
+		
+		for(int i = 0; i < arr.length; i++ ) {
+			
+				System.out.println("7*" + arr[i] + "=" + 7*arr[i]);
+			
+			}
+			
+		}
+	
+
+}
